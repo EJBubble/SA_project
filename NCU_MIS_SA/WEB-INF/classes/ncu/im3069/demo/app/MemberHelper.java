de7a561a705babaf506cadf493055697baccf154
EJBubble <JJ22507109@gmail.com>
2022-01-12 23:20:16 +0800

    login

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/MemberHelper.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/MemberHelper.java
@@ -549,5 +549,127 @@ public class MemberHelper {
             DBMgr.close(pres, conn);
         }
     }
+    
+    public boolean checkLogin(String email, String password){
+        /** 紀錄SQL總行數，若為「-1」代表資料庫檢索尚未完成 */
+        int row = -1;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT COUNT(*) FROM `saproject`.`member` WHERE `member_email` = ? AND `member_password` = ?";
+            
+            /** 取得所需之參數 */
+            String mail = email;
+            String Password = password;
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setString(1, mail);
+            pres.setNString(2, Password);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 讓指標移往最後一列，取得目前有幾行在資料庫內 */
+            rs.next();
+            row = rs.getInt("count(*)");
+            System.out.print(row);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+        
+        /** 
+         * 判斷是否已經有一筆該電子郵件信箱之資料
+         * 若無一筆則回傳False，否則回傳True 
+         */
+        return (row == 0) ? false : true;
+    }
+    
+    public JSONObject getByEmail(String Myemail) {
+        /** 新建一個 Member 物件之 m 變數，用於紀錄每一位查詢回之會員資料 */
+        Member m = null;
+        /** 用於儲存所有檢索回之會員，以JSONArray方式儲存 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `saproject`.`member` WHERE `member_email` = ? LIMIT 1";
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setString(1, Myemail);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+            
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            /** 正確來說資料庫只會有一筆該會員編號之資料，因此其實可以不用使用 while 迴圈 */
+            while(rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                row += 1;
+                
+                /** 將 ResultSet 之資料取出 */
+                int member_id = rs.getInt("id");
+                String name = rs.getString("member_name");
+                String password = rs.getString("member_password");
+                String email = rs.getString("member_email");
+                String phone = rs.getString("member_phone");
+                int login_times = rs.getInt("login_times");
+                
+                /** 將每一筆會員資料產生一名新Member物件 */
+                m = new Member(member_id, email, password, name, login_times, phone);
+                /** 取出該名會員之資料並封裝至 JSONsonArray 內 */
+                jsa.put(m.getData());
+            }
+            
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+        
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+        
+        /** 將SQL指令、花費時間、影響行數與所有會員資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
 
 }
---
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/LoginController.java
@@ -0,0 +1,84 @@
+package ncu.im3069.demo.controller;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import org.json.*;
+import ncu.im3069.demo.app.Member;
+import ncu.im3069.demo.app.MemberHelper;
+import ncu.im3069.tools.JsonReader;
+
+public class LoginController extends HttpServlet {
+    
+    /** The Constant serialVersionUID. */
+    private static final long serialVersionUID = 1L;
+    
+    /** mh，MemberHelper之物件與Member相關之資料庫方法（Sigleton） */
+    private MemberHelper mh =  MemberHelper.getHelper();
+    
+    /**
+     * 處理Http Method請求POST方法（新增資料）
+     *
+     * @param request Servlet請求之HttpServletRequest之Request物件（前端到後端）
+     * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
+     * @throws ServletException the servlet exception
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    
+    /**
+     * 處理Http Method請求GET方法（取得資料）
+     *
+     * @param request Servlet請求之HttpServletRequest之Request物件（前端到後端）
+     * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
+     * @throws ServletException the servlet exception
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+        JsonReader jsr = new JsonReader(request);
+        JSONObject jso = jsr.getObject();
+        
+        String email = jsr.getParameter("email");
+        
+        /** 若直接透過前端AJAX之data以key=value之字串方式進行傳遞參數，可以直接由此方法取回資料 */
+        String password = jsr.getParameter("password");
+        
+        
+        /** 判斷該字串是否存在，若存在代表要取回個別會員之資料，否則代表要取回全部資料庫內會員之資料 */
+        if (email.isEmpty() && password.isEmpty()) {
+            /** 透過MemberHelper物件之getAll()方法取回所有會員之資料，回傳之資料為JSONObject物件 */
+        	String resp = "{\"status\": \'400\', \"message\": \'登入失敗\', \'response\': \'\'}";	
+            /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+            jsr.response(resp, response);
+        }
+        else {
+            /** 透過MemberHelper物件的getByID()方法自資料庫取回該名會員之資料，回傳之資料為JSONObject物件 */
+            
+            boolean check = mh.checkLogin(email, password);
+            if (check == true)
+            {
+            	JSONObject query = mh.getByEmail(email);
+                
+                /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                JSONObject resp = new JSONObject();
+                resp.put("status", "200");
+                resp.put("message", "會員資料取得成功");
+                resp.put("response", query);
+        
+                /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                jsr.response(resp, response);
+            }
+            else
+            {
+            	String resp = "{\"status\": \'400\', \"message\": \'登入失敗\', \'response\': \'\'}";	
+                /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                jsr.response(resp, response);
+            }
+    
+           
+        }
+    }
+
+}
